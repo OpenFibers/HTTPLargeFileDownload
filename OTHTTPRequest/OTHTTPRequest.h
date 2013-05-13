bfe003a0009f4c219006cbd5208d50d25f45bf9a
openthread <openfibers@gmail.com>
2013-05-13 22:46:44 +0800

    finished OTHTTPRequest implementation

--- OTHTTPRequest/OTHTTPRequest.h
+++ OTHTTPRequest/OTHTTPRequest.h
@@ -8,6 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
+@class OTHTTPRequest;
+
+@protocol OTHTTPRequestDelegate <NSObject>
+
+- (void)otRequestFinished:(OTHTTPRequest *)request;
+- (void)otRequestFailed:(OTHTTPRequest *)request;
+
+@end
+
 @interface OTHTTPRequest : NSObject
+
+//Create request with a NSURLRequest.
+- (id)initWithNSURLRequest:(NSURLRequest *)request;
+
+@property (nonatomic, assign) id<OTHTTPRequestDelegate> delegate;
+
+@property (nonatomic,readonly) NSURLRequest *request;
+
+//If haven't receive response yet, return nil.
+@property (nonatomic,readonly) NSURLResponse *response;
+
+//Set if this is a low priority request. Set this property before call `start` to take effect.
+//Default value is `YES`.
+//When set to `NO`, request will be started at default priority.
+@property (nonatomic,assign) BOOL isLowPriority;
+
+//cancel request
+- (void)cancel;
+
+//begin request
+- (void)start;
+
+//Get responsed data
+- (NSData *)responseData;
 
 @end
--- OTHTTPRequest/OTHTTPRequest.m
+++ OTHTTPRequest/OTHTTPRequest.m
@@ -8,6 +8,114 @@
 
 #import "OTHTTPRequest.h"
 
+@interface OTHTTPRequest()<NSURLConnectionDataDelegate>
+@end
+
 @implementation OTHTTPRequest
+{
+    NSURLRequest *_request;
+    NSURLResponse *_response;
+    
+    NSMutableData *_data;
+    NSURLConnection *_connection;
+}
+
+//Create request with a NSURLRequest.
+- (id)initWithNSURLRequest:(NSURLRequest *)request
+{
+    self = [super init];
+    if (self)
+    {
+        _request = request;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self cancel];
+}
+
+- (NSURLRequest *)request
+{
+    return _request;
+}
+
+- (NSURLResponse *)response
+{
+    return _response;
+}
+
+- (void)cancel
+{
+    [_connection cancel];
+    _connection = nil;
+    _data = nil;
+    _response = nil;
+}
+
+- (void)start
+{
+    if (![NSThread isMainThread])
+    {
+        [self performSelectorOnMainThread:@selector(start) withObject:nil waitUntilDone:NO];
+        return;
+    }
+    
+    if (!_connection)
+    {
+        [self beginConnection];
+    }
+}
+
+- (void)beginConnection
+{
+    _data = [NSMutableData data];
+    _connection = [[NSURLConnection alloc] initWithRequest:_request delegate:self startImmediately:NO];
+    if (!self.isLowPriority)
+    {
+        [_connection scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+    }
+    [_connection start];
+}
+
+- (NSData *)responseData
+{
+    if (_data == nil)
+    {
+        return nil;
+    }
+    NSData *returnData = [NSData dataWithData:_data];
+    return returnData;
+}
+
+#pragma mark - NSURLConnectionDataDelegate Callbacks
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    _response = response;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [_data appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    if ([self.delegate respondsToSelector:@selector(otRequestFinished:)])
+    {
+        [self.delegate otRequestFinished:self];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    if ([self.delegate respondsToSelector:@selector(otRequestFailed:)])
+    {
+        [self.delegate otRequestFailed:self];
+    }
+    [self cancel];
+}
 
 @end
