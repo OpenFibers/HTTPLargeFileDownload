afdb64bb0c43fa246ffbe7001e878c99182cfe4f
OpenThread <openfibers@gmail.com>
2016-02-19 17:48:06 +0800

    added a class for post object

---
+++ OTHTTPRequest/PrivateClasses/OTHTTPRequestPostObject.h
@@ -0,0 +1,31 @@
+//
+//  OTHTTPRequestPostObject.h
+//  OTHTTPRequestDemo
+//
+//  Created by openthread on 2/19/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTHTTPRequestPostObject : NSObject
+
+//post param key
+@property (nonatomic, strong) NSString *key;
+
+//post param value. if post object is a string value, leave fileName MIMEType fileData and filePath nil.
+@property (nonatomic, strong) NSString *value;
+
+//if object is a file, this should be file name
+@property (nonatomic, strong) NSString *fileName;
+
+//if object is a file, this should be MIME type
+@property (nonatomic, strong) NSString *MIMEType;
+
+//if object is a file and send from memory, set data to this property and leave filePath nil
+@property (nonatomic, strong) NSData *fileData;
+
+//if object is a file and send from disk, set file path to this property and leave fileData nil
+@property (nonatomic, strong) NSString *filePath;
+
+@end
---
+++ OTHTTPRequest/PrivateClasses/OTHTTPRequestPostObject.m
@@ -0,0 +1,13 @@
+//
+//  OTHTTPRequestPostObject.m
+//  OTHTTPRequestDemo
+//
+//  Created by openthread on 2/19/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import "OTHTTPRequestPostObject.h"
+
+@implementation OTHTTPRequestPostObject
+
+@end
