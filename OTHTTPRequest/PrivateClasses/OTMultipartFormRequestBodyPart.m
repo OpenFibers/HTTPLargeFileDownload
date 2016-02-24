5d8e84c1ccddbd1105baedf96af51c0bd52e4173
OpenThread <openfibers@gmail.com>
2016-02-24 23:38:10 +0800

    body part read method finished

--- OTHTTPRequest/PrivateClasses/OTMultipartFormRequestBodyPart.m
+++ OTHTTPRequest/PrivateClasses/OTMultipartFormRequestBodyPart.m
@@ -11,6 +11,8 @@
 @interface OTMultipartFormRequestBodyPart()
 @property (nonatomic, strong) NSData *data;
 @property (nonatomic, strong) NSString *filePath;
+@property (nonatomic, assign) unsigned long long bytesHasRead;
+@property (nonatomic, strong) NSInputStream *fileReadStream;
 @end
 
 @implementation OTMultipartFormRequestBodyPart
@@ -78,14 +80,56 @@
     return self;
 }
 
+- (void)dealloc
+{
+    [self resetReadHandleAndOffset];
+}
+
 - (void)resetReadHandleAndOffset
 {
-    
+    [self.fileReadStream close];
+    self.fileReadStream = nil;
+    self.bytesHasRead = 0;
 }
 
 - (NSInteger)read:(uint8_t *)buffer maxLength:(NSUInteger)length
 {
+    if (self.data)
+    {
+        return [self readData:buffer maxLength:length];
+    }
+    else
+    {
+        return [self readFile:buffer maxLength:length];
+    }
     return 0;
+}
+
+- (NSInteger)readData:(uint8_t *)buffer maxLength:(NSUInteger)length
+{
+    NSUInteger rangeLength = MIN(self.length - self.bytesHasRead, length);
+    NSRange readRange = NSMakeRange(self.bytesHasRead, rangeLength);
+    [self.data getBytes:buffer range:readRange];
+    self.bytesHasRead += rangeLength;
+    return rangeLength;
+}
+
+- (NSInteger)readFile:(uint8_t *)buffer maxLength:(NSUInteger)length
+{
+    if (!self.fileReadStream)
+    {
+        self.fileReadStream = [[NSInputStream alloc] initWithFileAtPath:self.filePath];
+        [self.fileReadStream open];
+    }
+    
+    NSInteger bytesRead = [self.fileReadStream read:buffer maxLength:length];
+    self.bytesHasRead += bytesRead;
+    if (self.bytesHasRead == self.length)
+    {
+        [self.fileReadStream close];
+    }
+    
+    return bytesRead;
 }
 
 @end
