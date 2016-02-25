daf058c31911a57024c8315ec10cb7d642afca1f
OpenThread <openfibers@gmail.com>
2016-02-25 15:36:46 +0800

    added living request container

---
+++ OTHTTPRequest/PrivateClasses/OTLivingRequestContainer/OTLivingRequestContainer.h
@@ -0,0 +1,15 @@
+//
+//  OTLivingRequestContainer.h
+//  OTHTTPRequestDemo
+//
+//  Created by openthread on 2/25/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTLivingRequestContainer : NSObject
+
++ (instancetype)sharedContainer;
+
+@end
---
+++ OTHTTPRequest/PrivateClasses/OTLivingRequestContainer/OTLivingRequestContainer.m
@@ -0,0 +1,53 @@
+//
+//  OTLivingRequestContainer.m
+//  OTHTTPRequestDemo
+//
+//  Created by openthread on 2/25/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import "OTLivingRequestContainer.h"
+
+@interface OTLivingRequestContainer ()
+@property (nonatomic, strong) NSMutableArray *containerArray;
+@end
+
+@implementation OTLivingRequestContainer
+
++ (instancetype)sharedContainer
+{
+    static id container = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        container = [[self alloc] init];
+    });
+    return container;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        self.containerArray = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)addRequest:(id)request
+{
+    if (![self.containerArray containsObject:request])
+    {
+        [self.containerArray addObject:request];
+    }
+}
+
+- (void)removeRequest:(id)request
+{
+    if ([self.containerArray containsObject:request])
+    {
+        [self.containerArray removeObject:request];
+    }
+}
+
+@end
